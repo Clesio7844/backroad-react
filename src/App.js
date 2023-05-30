312449b698dee231a30993cdfd17e956a8e3b729
david <daviddecastro42@live.co.uk>
2023-05-30 01:01:57 +0100

    add component navbar about service hero tours footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Tours from './components/Tours';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <Navbar />
+    <Hero />
+    <About />
+    <Services />
+    <Tours />
+    <Footer />
+  
+    </>
   )
 }
 
