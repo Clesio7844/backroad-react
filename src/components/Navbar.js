3a435a2323973d5eefac469496abd2f63e50f5c6
david <daviddecastro42@live.co.uk>
2023-06-01 15:46:59 +0100

    convert html to react js component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../images/logo.svg'
+import { pageLinks, socialLinks } from '../data';
 
  const Navbar = () => {
   return (
@@ -13,39 +14,33 @@ import logo from '../images/logo.svg'
         </div>
         {/* <!-- left this comment on purpose --> */}
         <ul className="nav-links" id="nav-links">
-          <li>
-            <a href="#home" className="nav-link"> home </a>
+        {pageLinks.map((link) => {
+          return (
+            <li key={link.id}>
+            <a href={link.href} className="nav-link"> 
+            {link.text} </a>
           </li>
-
-          <li>
-            <a href="#about" className="nav-link"> about </a>
-          </li>
-
-          <li>
-            <a href="#services" className="nav-link"> services </a>
-          </li>
-
-          <li>
-            <a href="#tours" className="nav-link"> tours</a>
-          </li>
+          )
+        })}
         </ul>
 
+         
+
         <ul className="nav-icons">
-          <li>
-            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-              ><i className="fab fa-facebook"></i></a>
+        {socialLinks.map((link) =>{
+          const {id,href,icon} = link
+          return (
+            <li key={id}>
+            <a href={href} target="_blank" rel="noreferrer" className="nav-icon"
+              ><i className={icon}></i></a>
           </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-              ><i className="fab fa-twitter"></i></a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-              ><i className="fab fa-squarespace"></i></a>
-          </li>
+          
+          );
+        })}
+           
         </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 export default Navbar;
